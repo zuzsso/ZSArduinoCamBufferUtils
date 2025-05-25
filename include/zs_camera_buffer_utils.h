b087d0675ceb649c7c4e66171e30b588716e9929
nico santamaria <nico.santamaria@whistl.co.uk>
2025-05-25 18:16:01 +0100

    5x7 font

--- include/zs_camera_buffer_utils.h
+++ include/zs_camera_buffer_utils.h
@@ -7,12 +7,13 @@ namespace ZS
 {
     namespace CameraBufferUtils
     {
-        void drawLine(ZS::ImaGeometry::ImageLine * line, camera_fb_t *frame, uint16_t color);
-        void drawCircle(ZS::ImaGeometry::Circle * circle, camera_fb_t *frame, uint16_t color);
-        void drawRectangle(ZS::ImaGeometry::Rectangle * rectangle, camera_fb_t *frame, uint16_t color);
-        void draw5x3Text(ZS::ImaGeometry::ImageCoordinate * coordinate, const char *text, camera_fb_t *frame, uint16_t color);
-        void drawConvexArea(ZS::ImaGeometry::ConvexArea * area, camera_fb_t *frame, uint16_t color);
-        uint8_t getGrayscaleIndex(camera_fb_t *frame, int x, int y);        
+        void drawLine(ZS::ImaGeometry::ImageLine *line, camera_fb_t *frame, uint16_t color);
+        void drawCircle(ZS::ImaGeometry::Circle *circle, camera_fb_t *frame, uint16_t color);
+        void drawRectangle(ZS::ImaGeometry::Rectangle *rectangle, camera_fb_t *frame, uint16_t color);
+        void draw5x3Text(ZS::ImaGeometry::ImageCoordinate *coordinate, const char *text, camera_fb_t *frame, uint16_t color);
+        void draw5x7Text(ZS::ImaGeometry::ImageCoordinate *coordinate, const char *text, camera_fb_t *frame, uint16_t color);
+        void drawConvexArea(ZS::ImaGeometry::ConvexArea *area, camera_fb_t *frame, uint16_t color);
+        uint8_t getGrayscaleIndex(camera_fb_t *frame, int x, int y);
     }
 }
 #endif
